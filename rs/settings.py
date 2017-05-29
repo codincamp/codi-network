770ce11bb2ea0233b8e83acefeee2a9f4e3bcfb9
Alban atiberghien Tiberghien <alban.tiberghien@gmail.com>
2017-05-29 09:40:40 +0200

    Add homepage template

--- rs/settings.py
+++ rs/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'rs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'rs', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
