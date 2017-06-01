8684d2dbd597ff482cbf69279855c4e8d04f88c7
Alban Tiberghien <alban.tiberghien@gmail.com>
2017-06-01 09:34:56 +0200

    Implements profile model

--- people/models.py
+++ people/models.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField()
+    addr = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=5)
+    city = models.CharField(max_length=100)
+    website = models.CharField(max_length=100)
+    skills = models.CharField(max_length=500)
+    interests = models.CharField(max_length=500)
